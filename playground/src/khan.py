507327f6a6634622e38eb652e4b28f9b51f88bc8
spagbol <38818825+pozorfluo@users.noreply.github.com>
2020-04-06 00:40:00 +0200

    Add cachegrind output file to .gitignore

---
+++ playground/src/khan.py
@@ -0,0 +1,17 @@
+last_digits = set()
+
+for n in range(100):
+    last_digits.add(n%10)
+
+
+print(last_digits)
+
+
+import math
+math.sqrt(2) * math.sqrt(13)
+math.sqrt(26)
+
+4**(1/2)
+4**(-2)
+4**2
+1/16
